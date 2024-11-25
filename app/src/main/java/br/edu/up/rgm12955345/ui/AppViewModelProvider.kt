9d62cead5a0dd20640b877695cd73ee39d00ba64
agoecking <albrechtgoecking@gmail.com>
2024-11-25 12:37:01 -0300

    Updated the AppViewModelProvider

--- app/src/main/java/br/edu/up/rgm12955345/ui/AppViewModelProvider.kt
+++ app/src/main/java/br/edu/up/rgm12955345/ui/AppViewModelProvider.kt
@@ -8,39 +8,39 @@ import androidx.lifecycle.createSavedStateHandle
 import androidx.lifecycle.viewmodel.CreationExtras
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
+import br.edu.up.rgm12955345.InventoryApplication
+import br.edu.up.rgm12955345.ui.home.HomeViewModel
+import br.edu.up.rgm12955345.ui.item.ItemDetailsViewModel
+import br.edu.up.rgm12955345.ui.item.ItemEditViewModel
 import br.edu.up.rgm12955345.ui.item.ItemEntryViewModel
 
 object AppViewModelProvider {
     val Factory = viewModelFactory {
-        // Other Initializers
+        // Initializer for ItemEditViewModel
+        initializer {
+            ItemEditViewModel(
+                this.createSavedStateHandle()
+            )
+        }
         // Initializer for ItemEntryViewModel
-//        initializer {
-//            ItemEditViewModel(
-//                this.createSavedStateHandle()
-//            )
-//        }
-        // Initializer for ItemEntryViewModel
-//        initializer {
-//            ItemEntryViewModel()
-//        }
+        initializer {
+            ItemEntryViewModel(inventoryApplication().container.itemsRepository)
+        }
 
         // Initializer for ItemDetailsViewModel
-//        initializer {
-//            ItemDetailsViewModel(
-//                this.createSavedStateHandle()
-//            )
-//        }
+        initializer {
+            ItemDetailsViewModel(
+                this.createSavedStateHandle()
+            )
+        }
 
         // Initializer for HomeViewModel
-//        initializer {
-//            HomeViewModel()
-//        }
-//        initializer {
-//            ItemEntryViewModel(inventoryApplication().container.itemsRepository)
-//        }
-        //...
+        initializer {
+            HomeViewModel()
+        }
+
     }
 }
 
-//fun CreationExtras.inventoryApplication(): InventoryApplication =
-//    (this[AndroidViewModelFactory.APPLICATION_KEY] as InventoryApplication)
+fun CreationExtras.inventoryApplication(): InventoryApplication =
+    (this[AndroidViewModelFactory.APPLICATION_KEY] as InventoryApplication)
